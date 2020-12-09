be1bf977d8f45f5b37f348f2be423a8717051702
sudan bhusal <sudanbhusal20215@gmail.com>
2020-12-09 14:19:45 -0800

    file written

---
+++ index.js
@@ -0,0 +1,5 @@
+// include node fs module
+const fs = require("fs");
+let inquirer = require("inquirer");
+let question = require("./utils/question");
+let formatReadMeFile = require("./utils/formatMarkdown");
